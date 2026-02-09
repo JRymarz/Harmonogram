2c6b89bcc9fa78a201977c1034c5c1b77a788d26
JRymarz <jakubrymarz1@gmail.com>
2026-02-09 08:12:33 +0100

    Ukończono tworzenie insertów dla obu plików excela zawierających dane z miasta.

--- miasto.py
+++ miasto.py
@@ -2,6 +2,7 @@ import place_map
 import datetime
 import pandas as pd
 from pathlib import Path
+import re
 
 import date_service
 import insert_service
@@ -11,26 +12,57 @@ GMINA_ID = place_map.PLACE_MAP['Miasto Zamość']
 date = datetime.datetime.now()
 months = ['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII']
 
-def run():
-    project_root = Path(__file__).parent
-    excel_path = project_root / 'Data' / 'Miasto.xlsx'
+def run(variant):
 
-    data_excel = pd.read_excel(excel_path, header=3)
-    data_excel[['ULICE']] = (
-        data_excel[['ULICE']].ffill()
-    )
+    match variant:
+        case 1:
+            project_root = Path(__file__).parent
+            excel_path = project_root / 'Data' / 'Miasto1.xlsx'
 
-    mask = data_excel['Rodzaj odpadów'].isna() & data_excel['Unnamed: 1'].notna()
-    data_excel.loc[mask, 'Rodzaj odpadów'] = data_excel.loc[mask, 'Unnamed: 1']
-    data_excel = data_excel.drop(columns=['Unnamed: 1'])
+            data_excel = pd.read_excel(excel_path, header=3)
+            data_excel[['ULICE']] = (
+                data_excel[['ULICE']].ffill()
+            )
 
-    print(data_excel[['Rodzaj odpadów']])
+            # Jesli kilka ulic wpisane w jednej komorce dziele na oddzielne wiersze
+            data_excel['ULICE'] = (
+                data_excel['ULICE']
+                    .astype(str)
+                    .str.split(r'\s*,\s*')
+            )
+            data_excel = data_excel.explode('ULICE')
 
-    data_excel['daty_odbioru'] = data_excel.apply(date_service.build_dates, axis=1)
+            data_excel = data_excel.dropna(subset=months, how='all')
 
-    # print(data_excel[['ULICE', 'Rodzaj odpadów', 'daty_odbioru']])
+            data_excel['daty_odbioru'] = data_excel.apply(date_service.build_dates, axis=1)
 
-    # for _, row in data_excel.iterrows():
-    #     insert = insert_service.build_insert(row, GMINA_ID)
-    #     if insert:
-    #         print(insert)
+            for _, row in data_excel.iterrows():
+                insert = insert_service.build_insert(row, GMINA_ID)
+                if insert:
+                    print(insert)
+
+        case 2:
+            project_root = Path(__file__).parent
+            excel_path = project_root / 'Data' / 'Miasto2.xlsx'
+
+            data_excel = pd.read_excel(excel_path, header=1)
+            data_excel['ULICE'] = (
+                data_excel['ULICE'].ffill()
+            )
+
+            # Jesli kilka ulic wpisane w jednej komorce dziele na oddzielne wiersze
+            data_excel['ULICE'] = (
+                data_excel['ULICE']
+                .astype(str)
+                .str.split(r'\s*,\s*')
+            )
+            data_excel = data_excel.explode('ULICE')
+
+            data_excel = data_excel.dropna(subset=months, how='all')
+
+            data_excel['daty_odbioru'] = data_excel.apply(date_service.build_dates, axis=1)
+
+            for _, row in data_excel.iterrows():
+                insert = insert_service.build_insert(row, GMINA_ID)
+                if insert:
+                    print(insert)
